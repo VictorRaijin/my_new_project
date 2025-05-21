67db42c9f371d5677bc4771f030e44056f010c22
unknown <simeunovica39@gmail.com>
2025-05-21 12:00:00 +0300

    Added commit_by_date_recursive.bat

---
+++ commit_by_date_recursive.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+for /R %%F in (*) do (
+  for /f "tokens=1-4 delims=/ " %%a in ("%%~tF") do (
+    set file_date=%%c-%%a-%%bT12:00:00
+    echo Committing %%F with date !file_date!
+    git add "%%F"
+    set GIT_AUTHOR_DATE=!file_date!
+    set GIT_COMMITTER_DATE=!file_date!
+    git commit -m "Added %%~nxF"
+  )
+)
+
+endlocal
